d4ef4030694fdb2d3f8d89f0bdc5367bca4aeba8
Leandro Albanez <leandrorezende89@gmail.com>
2021-02-01 13:43:29 -0300

    Add Quiz Page

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,49 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-export const QuizContainer = styled.div`
-  width:100%;
-  max-width:350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+
+import InputBase from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Leandro Quiz</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The legend of CSS</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>loren i</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <InputBase
+                name="nomeDoUsuario"
+                onChange={(event) => setName(event.target.value)}
+                placeholder="Digite seu nome aqui"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -33,7 +51,7 @@ export default function Home() {
             <h1>Quizes da Galera</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>loren ipson is a dummy</p>
+            <p>loren ipson is a dummy text</p>
           </Widget.Content>
         </Widget>
         <Footer />
